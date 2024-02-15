cdf72d2a0f5fb99fc1d43dc9e37c3e32c5a0b1e2
Tushar Rawat <tusharrawat52@gmail.com>
2024-02-15 20:03:52 +0530

    fix: ACAO

--- index.ts
+++ index.ts
@@ -7,12 +7,15 @@ import {Server} from 'socket.io'
 dotenv.config();
 const app = express();
 
-
 app.use(cors({
     origin : "http://localhost:5173",
     methods : ["GET" , "POST"],
     credentials : true
 }));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    next();
+  });
 app.use(express.json());
 
 app.use('/' , IndexRouter);
@@ -21,6 +24,7 @@ app.use('/' , IndexRouter);
 const server = app.listen(process.env.PORT , () => {
     console.log(`The server is up on port : ${process.env.PORT}`);
 })
+
 
 const io = new Server(server , {
       cors : {
