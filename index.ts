d347c11fc0e728cbad7e5303e39f42c8b7e86121
Tushar Rawat <tusharrawat52@gmail.com>
2024-02-15 19:40:07 +0530

    fix: cors error -01

--- index.ts
+++ index.ts
@@ -8,7 +8,11 @@ dotenv.config();
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+    origin : "http://localhost:5173",
+    methods : ["GET" , "POST"],
+    credentials : true
+}));
 app.use(express.json());
 
 app.use('/' , IndexRouter);
